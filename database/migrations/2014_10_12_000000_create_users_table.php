f8d3a3db30f1df57a2cbea5f45127e9877a72aa4
Darkfafi <ik_ben_ramses@hotmail.com>
2018-11-22 22:59:40 +0100

    Removed welcome view, fixed string migration error, added profile picture support to user table in migration, added default user image, styled home & login views, placed header in separate file and included it in the layout, added base & redirect routes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table)
+        {
             $table->increments('id');
             $table->string('name');
+            $table->string('profile_picture_url')->default("#");
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-lg-12">
+            <div class="card bg-dark text-light">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -14,7 +13,6 @@
                         </div>
                     @endif
 
-                    You are logged in!
                 </div>
             </div>
         </div>
